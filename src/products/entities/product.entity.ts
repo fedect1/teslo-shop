d55e9c18b9168fd3e6f039aa54a58fd558226a20
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2024-01-12 09:23:26 +0000

    Add BeforeInsert function for Slug

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -41,5 +41,19 @@ export class Product {
 
     //tags
     //images
+
+    @BeforeInsert()
+    checkSlugInsert() {
+
+        if ( !this.slug ) {
+            this.slug = this.title
+        }
+        
+        this.slug = this.slug
+        .toLowerCase()
+        .replaceAll(' ','_')
+        .replaceAll("'",'')
+        
+    }
 
 }
