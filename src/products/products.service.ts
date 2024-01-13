fe797c2cdb0aa8ca21304a1f5151171c90197c5e
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2024-01-13 09:40:50 +0000

    Update findAll and findOne functionalities

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,8 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid'
 
 @Injectable()
 export class ProductsService {
@@ -25,16 +27,33 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return this.productRepository.find({})
+  findAll( paginationDto: PaginationDto ) {
+
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+    })
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id })
+  async findOne(term: string) {
+    let product: Product
+    if ( isUUID(term) ) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      //product = await this.productRepository.findOneBy({ slug: term });
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder.where('UPPER(title) =:title or slug =:slug', {
+          title: term.toUpperCase(),
+          slug: term.toLowerCase(),
+        }).getOne();
+    }
+
     if ( !product ) {
       throw new NotFoundException('Product not found')
     }
-    return product   
+    return product;   
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
