a74a2662ddfd8ab6bd2f2f59c92499c33c70b44a
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2024-01-13 13:37:10 +0000

    Modify Create Product Endpoint for adding Images

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid'
+import { ProductImage } from './entities';
 
 @Injectable()
 export class ProductsService {
@@ -14,14 +15,22 @@ export class ProductsService {
 
   constructor(
     @InjectRepository(Product)
-    private readonly productRepository: Repository<Product>
+    private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>,
+
   ) {}
 
   async create(createProductDto: CreateProductDto) {
     try {
-      const product = this.productRepository.create(createProductDto);
+      const { images = [], ...productDetails } = createProductDto;
+      const product = this.productRepository.create({
+        ...productDetails,
+        images: images.map( image => this.productImageRepository.create({ url: image }) ),
+      });
       await this.productRepository.save( product )
-      return product
+      return { ...product, images }
     } catch(error) {
       this.handleDBExceptions(error);
     }
@@ -59,7 +68,8 @@ export class ProductsService {
   async update(id: string, updateProductDto: UpdateProductDto) {
     const product = await this.productRepository.preload({
       id: id,
-      ...updateProductDto
+      ...updateProductDto,
+      images: [],
     });
     if ( !product ) throw new NotFoundException(`Product with id: ${ id } not found`);
     try {
