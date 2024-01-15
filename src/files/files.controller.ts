3000bf101cb012ca7590bca3c5749fc0f9914b7e
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2024-01-15 14:57:07 +0000

    Serving files in a controlled manner

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,13 +1,23 @@
-import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
 import { diskStorage } from 'multer';
 import { fileNamer } from './helpers/fileNamer.helper';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImg( 
+    @Param('imageName') imageName: string,
+    @Res() res: Response
+    ) {
+    const path = this.filesService.getStaticProductImage( imageName )
+    res.sendFile( path );
+  }
 
   @Post('product')
   @UseInterceptors( FileInterceptor('file', {
@@ -19,10 +29,10 @@ export class FilesController {
   }) )
   uploadProductImage( 
     @UploadedFile() file: Express.Multer.File ) {
-      if ( !file ) { throw new BadRequestException('Make sure that the file is an image') 
-    }  
+      if ( !file ) { throw new BadRequestException('Make sure that the file is an image')};
+      const secureUrl = `${file.filename}`  
       
-      return {filename: file.originalname}
+      return { secureUrl }
     
   }
 }
