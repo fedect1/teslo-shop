a4c5997709f43d06b4227cd88ba58a3cc9be82a6
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2024-01-19 08:14:20 +0000

    Authentication configuration with passport and asynchronous module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
@@ -10,7 +13,27 @@ import { User } from './entities/user.entity';
   imports: [
     TypeOrmModule.forFeature([
       User
-    ])
+    ]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports:[ ConfigModule ],
+      inject:[ ConfigService ],
+      useFactory: ( configService: ConfigService ) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h'
+          }
+        }
+      }
+
+    })
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '2h'
+    //   }
+    // })
   ], exports: [TypeOrmModule]
 })
 export class AuthModule {}
