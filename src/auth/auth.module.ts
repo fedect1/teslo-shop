ba350b4c47f63194e73b8b57405447f5f73b4020
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2024-01-22 09:55:20 +0000

    Create JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,11 +6,13 @@ import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([
       User
     ]),
@@ -28,12 +30,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }
 
     })
-    // JwtModule.register({
-    //   secret: process.env.JWT_SECRET,
-    //   signOptions: {
-    //     expiresIn: '2h'
-    //   }
-    // })
-  ], exports: [TypeOrmModule]
+  ], exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule]
 })
 export class AuthModule {}
